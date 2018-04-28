14d3b010af6cdac7e0f83ea5ddec823f54d634b8
ratbeknurlanov <ratbeknurlanov@gmail.com>
2018-04-28 19:40:53 +0500

    Character has updated

--- Model/Character.cs
+++ Model/Character.cs
@@ -6,8 +6,10 @@ namespace SaveTheCommunism.Model
     {
         public int Health { get; set; }
         public int Damage { get; set; }
-        public Vector Speed { get; set; }
+        public Vector Position { get; set; }
         public Vector Direction { get; set; }
+        public Vector Speed { get; set; }
+        public Vector Velocity { get; set; }
 
         public virtual void Hit(Character another)
         {
