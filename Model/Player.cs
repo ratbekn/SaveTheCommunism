94b352a2484be1194b9a002f2bb4702edf6e5390
ratbeknurlanov <ratbeknurlanov@gmail.com>
2018-05-02 17:37:43 +0500

    Djikstra class has created and Player has modified

--- Model/Player.cs
+++ Model/Player.cs
@@ -13,140 +13,17 @@ namespace SaveTheCommunism.Model
         {
         }
 
-        //переименовать, слишком общее название
-        //public int Ability { get; set; }
+        public int RecruitAbility { get; set; }
 
-        //public override void Hit(Character another)
-        //{
-        //    base.Hit(another);
-        //    Ability += 1;
-        //}
-
-        //public void Recruit(Enemy enemy)
-        //{
-
-        //}
-
-        public List<Point> GetPathToNearestEnemy(List<Enemy> enemies, Size squareSize)
+        public override void Hit(Character another)
         {
-            var currentPosition = Position.ToPoint();
-            var enemiesPositions = enemies
-                .Select(enemy => enemy.Position.ToPoint())
-                .ToList();
-            var pathToNextEnemy = GetPathToNearestEnemy(currentPosition, enemiesPositions, squareSize);
-            return pathToNextEnemy ?? new List<Point>(); 
+            base.Hit(another);
+            RecruitAbility += 1;
         }
 
-        private static List<Point> GetPathToNearestEnemy(Point initialPosition, List<Point> enemiesPositions, Size squareSize)
+        public void Recruit(Enemy enemy)
         {
-            var visited = new Dictionary<Point, DijkstraData>();
-            var track = new Dictionary<Point, DijkstraData>
-            {
-                {initialPosition, new DijkstraData(initialPosition, 0)}
-            };
 
-            while (track.Count != 0)
-            {
-                var toOpen = GetPointToOpen(track);
-
-                visited.Add(toOpen, track[toOpen]);
-                track.Remove(toOpen);
-
-                if (enemiesPositions.Contains(toOpen))
-                    return BuildPath(visited, initialPosition, toOpen);
-
-                AddNextPointsToTrack(toOpen, track, visited, squareSize);
-            }
-
-            return null;
-        }
-
-        private static Point GetPointToOpen(Dictionary<Point, DijkstraData> track)
-        {
-            var toOpen = default(Point);
-            var requiredEnergy = int.MaxValue;
-            foreach (var point in track.Keys)
-            {
-                if (track[point].Weight >= requiredEnergy)
-                    continue;
-                toOpen = point;
-                requiredEnergy = track[point].Weight;
-            }
-
-            return toOpen;
-        }
-
-        private static List<Point> BuildPath(Dictionary<Point, DijkstraData> track, Point start, Point end)
-        {
-            var result = new List<Point>();
-            while (end != start)
-            {
-                result.Add(end);
-                end = track[end].Previous;
-            }
-            result.Reverse();
-            return result;
-        }
-
-        private static void AddNextPointsToTrack(
-                Point toOpen,
-                Dictionary<Point, DijkstraData> track,
-                Dictionary<Point, DijkstraData> visited,
-                Size squareSize)
-        {
-            foreach (var nextPoint in GetNextPoints(toOpen))
-            {
-                if (IsNextPointInCorrect(visited, nextPoint, squareSize))
-                    continue;
-
-                var weight = visited[toOpen].Weight + 1;
-
-                if (!track.ContainsKey(nextPoint) || track[nextPoint].Weight > weight)
-                    track[nextPoint] = new DijkstraData(toOpen, weight);
-            }
-        }
-
-        private static bool IsNextPointInCorrect(Dictionary<Point, DijkstraData> visited, Point nextPoint, Size squareSize)
-        {
-            return visited.ContainsKey(nextPoint) || !InsideSquare(nextPoint, squareSize);
-        }
-
-        private static bool InsideSquare(Point point, Size squareSize)
-        {
-            return point.X >= 0 && point.Y >= 0 && point.X <= squareSize.Width && point.Y <= squareSize.Height;
-        }
-
-        private static readonly Size[] NextStepDirections = { new Size(0, -1), new Size(0, 1), new Size(-1, 0), new Size(1, 0) };
-
-        private static IEnumerable<Point> GetNextPoints(Point point)
-        {
-            return NextStepDirections.Select(size => new Point(point.X + size.Width, point.Y + size.Height));
-        }
-    }
-
-    class WeightedPath
-    {
-        public List<Point> Path { get; }
-        public int Weight { get; }
-        public Point First => Path.First();
-        public Point Last => Path[Path.Count - 1];
-
-        public WeightedPath(int weight)
-        {
-            Weight = weight;
-            Path = new List<Point>();
-        }
-    }
-
-    class DijkstraData
-    {
-        public Point Previous { get; }
-        public int Weight { get; }
-
-        public DijkstraData(Point previous, int weight)
-        {
-            Previous = previous;
-            Weight = weight;
         }
     }
 }
